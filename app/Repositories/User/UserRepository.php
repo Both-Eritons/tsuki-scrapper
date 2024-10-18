91d3bca00acc82df9323b405d3cf484c965cef2a
Eriton <botheritons2005@gmail.com>
2024-10-18 10:32:20 -0300

    feat: add UserRepository with badic methoda

---
+++ app/Repositories/User/UserRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repositories\User;
+
+use App\Models\User;
+use App\Repositories\User\Contracts\Contract;
+
+class UserRepository implements Contract
+{
+    public function __construct(
+        private User $user
+    ){}
+
+    public function findById(int $id): ?User
+    {
+        return $this->user::find($id);
+    }
+
+    public function findByEmail(string $email): ?User
+    {
+        return $this->user::where('email', $email)->first();
+    }
+
+    public function findByUsername(string $user): ?User
+    {
+        return $this->user::where('username', $user)->first();
+    }
+
+    public function create(array $data): ?User
+    {
+        return $this->user::create($data);
+    }
+
+    public function deleteById(int $id): ?User
+    {
+        return $this->user::find($id)->delete();
+    }
+
+    public function deleteByEmail(string $email): ?User
+    {
+        return $this->user::where('email', $email)->delete();
+    }
+}
