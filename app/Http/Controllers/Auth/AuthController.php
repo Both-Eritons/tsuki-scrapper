e7a10c10e834c50ab8163c401a69bbb52a6c8f2f
Eriton <botheritons2005@gmail.com>
2024-10-18 10:35:07 -0300

    feat: AuthController with methods to login, register, logout user

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+
+class AuthController extends AuthBase
+{
+    public function login()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
+}
