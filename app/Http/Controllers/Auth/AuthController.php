67562432caa835e82735fe811420e894e5035d39
Eriton <botheritons2005@gmail.com>
2024-10-19 05:52:11 -0300

    feat: Auth controller, using login action in method login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Actions\Auth\Login;
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 
 class AuthController extends AuthBase
 {
-    public function login()
+    public function login(LoginRequest $req, Login $action)
     {
+        $token = $action->execute($req->validated());
 
+        return $this->respondWithToken($token);
     }
 
     public function register()
