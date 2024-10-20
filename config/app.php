823fb29459d8b4182bf2115f8c10551feecb7f0a
Eriton <botheritons2005@gmail.com>
2024-10-20 05:52:51 -0300

    feat: add jwt auth in providers

--- config/app.php
+++ config/app.php
@@ -123,4 +123,8 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    'providers' => [
+        PHPOpenSourceSaver\JWTAuth\Providers\LaravelServiceProvider::class
+    ],
+
 ];
