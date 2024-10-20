ab85fb5748f36828b1edecb0ab3c86b3ce3e6362
Eriton <botheritons2005@gmail.com>
2024-10-20 05:53:49 -0300

    feat: user login action

--- app/Actions/Auth/Login.php
+++ app/Actions/Auth/Login.php
@@ -2,14 +2,22 @@
 
 namespace App\Actions\Auth;
 
+use App\Exceptions\Auth\Unauthorized;
+use App\Exceptions\User\UserNotExistsExeception;
+use App\Repositories\User\UserRepository;
+
 class Login
 {
     public function __construct(
         private UserRepository $user,
     ){}
 
-    public function execute(array $data)
+    public function execute(array $data): string
     {
-
+        $user = $this->user->findByEmail($data['email']);
+        if(!$user) throw new UserNotExistsExeception();
+        $token = auth()->guard()->attempt($data);
+        if(!$token) throw new Unauthorized();
+        return $token;
     }
 }
